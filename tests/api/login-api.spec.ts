89803e2969eb3864aa36fce8cb4bd75524eb2943
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-05-11 21:10:26 +0530

    Extract Response header form one request & set for another request headers

--- tests/api/login-api.spec.ts
+++ tests/api/login-api.spec.ts
@@ -6,6 +6,18 @@ test("Login to Application Using API", async ({ request }) => {
   });
   const apiRES = JSON.parse(await loginUser.text());
   console.log(apiRES);
-  console.log(apiRES.user);
-  console.log(apiRES.user.id);
+  const apiHEaders = loginUser.headers();
+
+  process.env["Cookie"] = apiHEaders["set-cookie"].split(";")[0];
+  console.log(process.env[`Cookie`]);
+});
+
+test("/bankAccounts", async ({ request }) => {
+  const loginUser = await request.get(`${process.env.API_URL}/bankAccounts`, {
+    headers: {
+      Cookie: process.env["Cookie"],
+    },
+  });
+  const apiRES = JSON.parse(await loginUser.text());
+  console.log(apiRES);
 });
