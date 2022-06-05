309cb3cba0a09a5c1e79be1edaabfe63c89052e9
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-06-05 08:55:30 +0530

    Add ROADMAP.md

---
+++ ROADMAP.md
@@ -0,0 +1,5 @@
+## Implemented
+
+## tobe Implemented
+
+1. UUID for Unique ID for Test Case or Test Automation Framework
--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@config/page.config";
 test("Login to Application", async ({ loginPage, page, logger }) => {
+  //Logger implementation
   logger.warn("Started");
   await loginPage.goto();
   await loginPage.performLogin();
