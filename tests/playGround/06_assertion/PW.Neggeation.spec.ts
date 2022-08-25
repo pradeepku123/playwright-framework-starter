e25392f574b4a61b18f2c16b65c2119a9666f038
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-08-25 20:44:42 +0530

    Assert-explre-001

--- tests/playGround/06_assertion/PW.Neggeation.spec.ts
+++ tests/playGround/06_assertion/PW.Neggeation.spec.ts
@@ -4,7 +4,7 @@ test(`test Assertion Negggeation`, async ({ page }) => {
   expect('TRUE').not.toEqual('TRUE');
 });
 
-test.only(`Soft Assertion`, async ({ page }) => {
+test(`Soft Assertion`, async ({ page }) => {
   //Failed Assertion will terminate execution. Playwright all so Soft Assertion.
   // Soft Assertion do not failed execution. but mark as test Failed
   await page.goto('http://localhost:3000');
@@ -12,4 +12,10 @@ test.only(`Soft Assertion`, async ({ page }) => {
   const TEXT = await page.locator('h1:has-text("Sign in")').innerText();
   console.log(TEXT);
   expect(TEXT, 'This was test Neggation').toEqual('Sign in');
+});
+
+test.only(``, async ({ page }) => {
+  await page.goto('https://example.cypress.io/commands/actions');
+  await page.locator('.action-checkboxes [type="checkbox"]').first().click();
+  await expect(page.locator('.action-checkboxes [type="checkbox"]').first()).toBeChecked();
 });
