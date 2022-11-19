f80ee5ae13c87c67fcf73db2caac3b40af4817e0
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-11-19 06:58:51 +0530

    Add Playwright Dependencies 1.28 resolve error with allure report

--- tests/playGround/11_lambdatest/lambdatest-explre.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from 'lambdatest-setup';
-// import { test } from '@playwright/test';
-
-test(`TEST`, async ({ page }) => {
-  await page.goto('https://example.cypress.io/commands/actions');
-  await page.locator('.action-checkboxes [type="checkbox"]').first().click();
-});
