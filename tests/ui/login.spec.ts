eb409ffb7249262ab5a27df83d3dcebb2c7df9eb
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-03-23 09:05:06 +0530

    PW update 1.32 with UI Mode

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -5,5 +5,5 @@ test('Login to Application', async ({ loginPage, page, logger, uuidVersion4 }) =
   await loginPage.goto();
   await loginPage.performLogin();
   await expect(page.locator('[data-test="sidenav-user-full-name"]')).toHaveText('Edgar J');
-  // await page.waitForTimeout(3000);
+  await page.waitForTimeout(3000);
 });
