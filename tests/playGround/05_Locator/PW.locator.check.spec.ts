866d56bca0754e017a51b454d5fbf7c4f434a9d3
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-05-20 08:21:58 +0530

    setup cloudeflare

--- tests/playGround/05_Locator/PW.locator.check.spec.ts
+++ tests/playGround/05_Locator/PW.locator.check.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
-test.only(`Locator:Check`, async ({ page }) => {
+test(`Locator:Check`, async ({ page }) => {
   await page.goto(`https://example.cypress.io/commands/actions`);
-  test.slow();
+  //   test.slow();
 
   //   await page.locator('.action-checkboxes [value="checkbox1"]').scrollIntoViewIfNeeded();
   await page.locator('.action-checkboxes [value="checkbox1"]').check();
@@ -15,5 +15,11 @@ test.only(`Locator:Check`, async ({ page }) => {
 
   await page.locator('.action-multiple-checkboxes [value="checkbox3"]').uncheck();
   await page.locator('.action-multiple-checkboxes [value="checkbox1"]').uncheck();
+  await page.waitForTimeout(3000);
+});
+test.only(`Locator:Count`, async ({ page }) => {
+  await page.goto(`https://example.cypress.io/commands/actions`);
+  const elementCount = await page.locator('.action-checkboxes [type="checkbox"]').count();
+  console.log(`EleC::` + elementCount);
   await page.waitForTimeout(3000);
 });
