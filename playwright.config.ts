c136ea79b14e98a753b36e03fff5e4c8f658dc33
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-07-21 16:08:26 +0530

    Add SnapshotDir

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ require('dotenv').config();
  */
 export default defineConfig({
   testDir: './tests',
+  snapshotDir: './snapshots',
   testMatch: ['*.spec.ts'],
   timeout: 10 * 1000,
   workers: 1,
